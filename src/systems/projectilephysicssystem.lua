940c8f0f33c3c0e4e07492d0ddeba967ddba5370
Matt Dannenberg <matt.dannenberg@teacherspayteachers.com>
2016-12-03 16:13:42 -0500

    last time yall!!

--- src/systems/projectilephysicssystem.lua
+++ src/systems/projectilephysicssystem.lua
@@ -14,7 +14,7 @@ function ProjectilePhysicsSystem:process(e, dt)
     e.pos.x = e.pos.x + e.xVelocity * dt
     e.pos.y = e.pos.y + e.yVelocity * dt
     if e.pos.x > 800 or e.pos.x < 0 then
-        e.isDead = true
+        world:remove(e)
     end
 end
 
