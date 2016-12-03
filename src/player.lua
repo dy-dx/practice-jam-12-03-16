619347fa7e4de9f2c58a77f69c2311e477ccf195
Chris Kim <christopherjkim@gmail.com>
2016-12-03 13:58:33 -0500

    add basic movement and a sprite

--- src/player.lua
+++ src/player.lua
@@ -1,27 +1,44 @@
 -- http://hump.readthedocs.io/en/latest/class.html
-local Player = Class{
-    init = function(self, pos, img)
-        self.pos = pos
-        self.img = img
-        self:reset()
-    end;
-}
+local Player = Class{}
 
-function Player:reset()
-end
-
-function Player:update(dt)
-end
-
-function Player:draw()
-end
-
-function Player:init(image)
+function Player:init()
+    self.image = love.graphics.newImage("assets/images/megaman_sample_sprite_right.png")
     self:reset()
 end
 
+function Player:reset()
+    self.x = 100
+    self.y = 500
+    self.xVelocity = 0
+    self.yVelocity = 0
+    self.xSpeed = 200
+    self.jumpVelocity = -200
+    self.gravity = 1000
+end
+
+function Player:draw()
+    love.graphics.draw(self.image, self.x, self.y)
+end
 
 function Player:update(dt)
+    self.yVelocity = self.yVelocity + self.gravity * dt
+
+    if love.keyboard.isDown("up") then
+        self.yVelocity = self.jumpVelocity
+    end
+
+    if love.keyboard.isDown("left") then
+        self.xVelocity = -self.xSpeed
+    elseif love.keyboard.isDown("right") then
+        self.xVelocity = self.xSpeed
+    else
+        self.xVelocity = 0
+    end
+
+    self.y = self.y + self.yVelocity * dt
+    self.x = self.x + self.xVelocity * dt
+    self.x = lume.clamp(self.x, 0, 800);
+    self.y = lume.clamp(self.y, 0, 500);
 end
 
 return Player
