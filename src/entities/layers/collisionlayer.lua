f3132d27a757fde621e7b4f1dccf565c1e88d30e
Chris Kim <christopherjkim@gmail.com>
2016-12-03 19:50:16 -0500

    add middlelayer tiles to bumpworld

--- src/entities/layers/collisionlayer.lua
+++ src/entities/layers/collisionlayer.lua
@@ -2,11 +2,23 @@ local layerUtils = require 'utils/layerutils'
 
 local CollisionLayer = Class{}
 
-function CollisionLayer:init()
-    self:reset()
-end
-
-function CollisionLayer:reset()
+function CollisionLayer:init(tileMap, bumpWorld)
+    for lindex, layer in ipairs(tileMap.layers) do
+        if layer.name == 'MiddleLayer' then
+            local prefix = layer.properties.oneway == "true" and "o(" or "t("
+            for y, tiles in ipairs(layer.data) do
+                for x, tile in pairs(tiles) do
+                    bumpWorld:add(
+                        prefix..layer.name..", "..x..", "..y..")",
+                        x * tileMap.tilewidth  + tile.offset.x,
+                        y * tileMap.tileheight + tile.offset.y,
+                        tile.width,
+                        tile.height
+                    )
+                end
+            end
+        end
+    end
 end
 
 function CollisionLayer:draw()
