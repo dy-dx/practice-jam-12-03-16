41e719591d82a5aad04c25dfa4cd265abc4cd50e
Greg <gregt87@gmail.com>
2016-12-03 19:34:26 -0500

    ialsdkfj

--- src/entities/layers/collisionlayer.lua
+++ src/entities/layers/collisionlayer.lua
@@ -1,9 +1,26 @@
 local layerUtils = require 'utils/layerutils'
+local CollisionBox = require 'entities/collisionbox'
 
 local CollisionLayer = Class{}
 
-function CollisionLayer:init()
+function CollisionLayer:init(map)
     self:reset()
+
+    printTable(layerUtils:getLayer('MiddleLayer'))
+    -- printTable(Global.map.layers)
+    printTable(layerUtils:getLayer('MiddleLayer').data)
+
+    print('width ' .. layerUtils:getLayer('MiddleLayer').width)
+    print('height ' .. layerUtils:getLayer('MiddleLayer').height)
+    print("properties")
+    printTable(layerUtils:getLayer('MiddleLayer').properties)
+
+    print("batches")
+    printTable(layerUtils:getLayer('MiddleLayer').batches)
+
+    -- world:addEntity(CollisionBox(0, 550, 800, 30))
+    -- world:addEntity(CollisionBox(0, 0, 40, 600))
+    -- world:addEntity(CollisionBox(700, 0, 40, 600))
 end
 
 function CollisionLayer:reset()
